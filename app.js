f661666d67592eb34744f3e95fefce30f1c8f76a
tsoupionis <thomassoup@yahoo.com>
2023-04-01 18:16:36 -0400

    Update to todo list 4/1

--- app.js
+++ app.js
@@ -3,19 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    var today = new Date(); 
+    let today = new Date(); 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-    var currentDay = today.getDay(); 
-    // https://www.w3schools.com/jsref/jsref_getday.asp
-    var weekDays= ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var dayOfWeek = weekDays[currentDay]; 
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    let dayOfWeek = today.toLocaleDateString("en-US",options);
+     
     
-    res.render("list", {kindOfDay: dayOfWeek});
+    res.render("list", {kindOfDay: dayOfWeek, newListItems: items});
     
 });
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    items.push(item); //  passes each new to do list item to the global "items" array
+
+    res.redirect("/"); // when post request is triggered, redirect to home route
+    // bringing you back to app.get("/", function(req, res) {
+
+})
 
 app.listen(3000, function() {
     console.log("Server listening on port 3000");
